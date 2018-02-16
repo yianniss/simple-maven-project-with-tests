db909facbfd7f54704c954971c26e33a888de5ee
yianniss <36532656+yianniss@users.noreply.github.com>
2018-02-16 12:20:29 +0200

    Update Base.java

--- src/test/java/test/Base.java
+++ src/test/java/test/Base.java
@@ -31,10 +31,8 @@ class Base {
 
     protected void run() {
         double r = Math.random();
-        if (r < 0.1) {
+        if (r < 0.0) {
             fail("oops");
-        } else if (r < 0.2) {
-            throw new AssumptionViolatedException("skipping");
         }
     }
 
